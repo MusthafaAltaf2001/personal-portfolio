ebea6cd32ff10e16a56ee355431aaac3664db4fb
MusthafaAltaf2001 <musthafaaltaf2001@gmail.com>
2023-09-09 01:25:07 +0800

    Added functionality for sending message

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
 
 const Footer = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+
+    const handleNameChange = (e) => {
+        setName(e.target.value)
+    }
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value)
+    }
+
+    const sendMessage = async () => {
+        const templateParams = {
+            from_name: name,
+            user_email: email,
+            message: message
+        };
+
+        const expressBackendURL = 'https://personal-portfolio-backend-s20c.onrender.com/sendMessage'
+
+        await fetch(expressBackendURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ templateParams }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Response from Express:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
     return (
         <div className="flex flex-col items-center mt-10 z-400 bg-[#111827]">
             <span id="footer" className="text-6xl font-bold text-[#EAB308] px-10 py-4">
@@ -26,10 +68,10 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className='flex flex-col'>
-                    <input className='bg-[#1F2937] text-[#F3F4F6] border-0 rounded p-2 w-[250px] m-2' type='text' name="myInput" placeholder='Name' />
-                    <input className='bg-[#1F2937] text-[#F3F4F6] border-0 rounded p-2 w-[250px] m-2' type='email' name="myInput" placeholder='Your email' />
-                    <input className='bg-[#1F2937] text-[#F3F4F6] border-0 rounded pl-2 pt-2 pb-[120px] w-[400px] m-2 text-justify' type='text' name="myInput" placeholder='Message' />
-                    <div className='w-40 h-1/12  rounded-md m-2 bg-[#EAB308] px-2 py-1 text-center items-center' >
+                    <input onChange={handleNameChange} value={name} className='bg-[#1F2937] text-[#F3F4F6] border-0 rounded p-2 w-[250px] m-2' type='text' name="myInput" placeholder='Name' />
+                    <input onChange={handleEmailChange} value={email} className='bg-[#1F2937] text-[#F3F4F6] border-0 rounded p-2 w-[250px] m-2' type='email' name="myInput" placeholder='Your email' />
+                    <input onChange={handleMessageChange} value={message} className='bg-[#1F2937] text-[#F3F4F6] border-0 rounded pl-2 pt-2 pb-[120px] w-[400px] m-2 text-justify' type='text' name="myInput" placeholder='Message' />
+                    <div onClick={sendMessage} className='w-40 h-1/12  rounded-md m-2 bg-[#EAB308] px-2 py-1 text-center items-center' >
                         <span className='text-[#000000]'>Send Message</span>
                     </div>
                 </div>
