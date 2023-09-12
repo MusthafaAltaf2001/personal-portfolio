92cd43d75adc3a981e362e7dc1d34212cd42643e
MusthafaAltaf2001 <musthafaaltaf2001@gmail.com>
2023-09-12 12:57:52 +0800

    Added clickable links for projects

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,13 +4,16 @@ import SkillsList from './SkillsList'
 
 
 const About = () => {
+    const aboutText =
+        "I'm a passionate software developer with three years of experience in creating software, mainly focusing on the frontend. I love learning and always strive to expand my knowledge in the software world. I find working on software projects exciting because it involves solving problems and using logic, which happens to be one of my strengths."
+
     return (
         <div id="about" className='flex flex-col items-center pt-16'>
             <span className="text-4xl font-bold text-[#EAB308] px-10 py-4">
                 ABOUT
             </span>
             <span className="text-xl text-[#F3F4F6] px-[50px] md:px-[200px] py-4 text-justify xl:px-[450px]">
-                Passionate software developer with 3 years experience in developing softwares with varying technologies. My experience is not only limited to software development, but also managing teams, JIRA boards and professional communication amongst clients. Passionate software developer with 3 years experience in developing softwares with varying technologies. My experience is not only limited to software development, but also managing teams, JIRA boards and professional communication amongst clients
+                {aboutText}
             </span>
             <SkillsList />
         </div>
--- src/Components/ProjectsList.jsx
+++ src/Components/ProjectsList.jsx
@@ -6,22 +6,28 @@ import reddit_clone_project from '../Assets/reddit_clone_project.png'
 const ProjectsList = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            <div className="projects-div">
-                <span className='projects-div-heading'>VISE - Visually Impaired Shopping Experience</span>
-                <img className='projects-div-img' src={vise_project} alt=' ' />
-                <span className='projects-div-description'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
-            </div>
-            <div className="projects-div">
-                <span className='projects-div-heading'>SIMP - Student Internship Management Program</span>
-                <img className='projects-div-img' src={simp_project} alt=' ' />
-                <span className='projects-div-description'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
-            </div>
-            <div className="projects-div">
-                <span className='projects-div-heading'>Reddit Clone</span>
-                <img className='projects-div-img' src={reddit_clone_project} alt=' ' />
-                <span className='projects-div-description'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
-            </div>
-        </div>
+            <a href="https://github.com/MusthafaAltaf2001/vise">
+                <div className="projects-div">
+                    <span className='projects-div-heading'>VISE - Visually Impaired Shopping Experience</span>
+                    <img className='projects-div-img' src={vise_project} alt=' ' />
+                    <span className='projects-div-description'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+                </div>
+            </a>
+            <a href="https://github.com/MusthafaAltaf2001/student_internship_management_program">
+                <div className="projects-div">
+                    <span className='projects-div-heading'>SIMP - Student Internship Management Program</span>
+                    <img className='projects-div-img' src={simp_project} alt=' ' />
+                    <span className='projects-div-description'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+                </div>
+            </a>
+            <a href="https://github.com/MusthafaAltaf2001/reddit-clone-1">
+                <div className="projects-div">
+                    <span className='projects-div-heading'>Reddit Clone</span>
+                    <img className='projects-div-img' src={reddit_clone_project} alt=' ' />
+                    <span className='projects-div-description'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+                </div>
+            </a>
+        </div >
     )
 }
 
